1dbea0920f1af2b44676e71005fde006e28b5140
rameshdayaka <dayakar2@mail.sacredheart.edu>
2024-02-01 03:10:49 -0500

    Classwork_2

--- Class_Work_2/1.assedningorder.cpp
+++ Class_Work_2/1.assedningorder.cpp
@@ -1,23 +1,35 @@
 // Online C++ compiler to run C++ program online
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int a[]={8,7,6,5,2,4,1,3,9};
+    // Initializing an array
+    int a[] = {8, 7, 6, 5, 2, 4, 1, 3, 9};
+    
+    // Calculating the length of the array
     int getArrayLength = sizeof(a) / sizeof(int);
-    for (int i = 0; i < getArrayLength; i++)//up until the length
+
+    // Bubble Sort Algorithm to sort the array in ascending order
+    for (int i = 0; i < getArrayLength; i++) // Iterating through each element
     {
-        for(int j = 0;j<getArrayLength-1; j++){//up until last but one 
-         if(a[j]>a[j+1]){
-             int temp = a[j];
-             a[j] = a[j+1];
-             a[j+1] = temp;
-             
-         }
+        for (int j = 0; j < getArrayLength - 1; j++) // Comparing adjacent elements
+        {
+            if (a[j] > a[j + 1]) {
+                // Swapping if the current element is greater than the next
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
     }
+
+    // Printing the sorted array
+    cout << "Sorted Array: ";
+    for (int i = 0; i < getArrayLength; i++) {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
 }
-//printing
-for (int i=0;i<getArrayLength;i++){
-    cout<<a[i];
-} 
-}
+
--- Class_Work_2/4.queue.cpp
+++ Class_Work_2/4.queue.cpp
@@ -1,39 +1,50 @@
 // CPP code to illustrate Queue in 
 // Standard Template Library (STL)
+
 #include <iostream>
 #include <queue>
 
 using namespace std;
 
-// Print the queue
-void showq(queue<int> gq)
-{
-	queue<int> g = gq;
-	while (!g.empty()) {
-		cout << '\t' << g.front();
-		g.pop();
-	}
-	cout << '\n';
+// Function to print the queue
+void showQueue(queue<int> gq) {
+    // Creating a copy of the original queue
+    queue<int> g = gq;
+    
+    // Printing the elements of the queue
+    while (!g.empty()) {
+        cout << '\t' << g.front();
+        g.pop();
+    }
+    cout << '\n';
 }
 
 // Driver Code
-int main()
-{
-	queue<int> gquiz;
-	gquiz.push(10);
-	gquiz.push(20);
-	gquiz.push(30);
+int main() {
+    // Creating a queue named 'gquiz'
+    queue<int> gquiz;
 
-	cout << "The queue gquiz is : ";
-	showq(gquiz);
+    // Adding elements to the queue
+    gquiz.push(10);
+    gquiz.push(20);
+    gquiz.push(30);
 
-	cout << "\ngquiz.size() : " << gquiz.size();
-	cout << "\ngquiz.front() : " << gquiz.front();
-	cout << "\ngquiz.back() : " << gquiz.back();
+    // Displaying the original queue
+    cout << "The queue gquiz is : ";
+    showQueue(gquiz);
 
-	cout << "\ngquiz.pop() : ";
-	gquiz.pop();
-	showq(gquiz);
+    // Displaying size, front, and back of the queue
+    cout << "\ngquiz.size() : " << gquiz.size();
+    cout << "\ngquiz.front() : " << gquiz.front();
+    cout << "\ngquiz.back() : " << gquiz.back();
 
-	return 0;
+    // Removing an element from the front of the queue
+    cout << "\ngquiz.pop() : ";
+    gquiz.pop();
+
+    // Displaying the modified queue
+    showQueue(gquiz);
+
+    return 0;
 }
+
